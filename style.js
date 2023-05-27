199c6b3b2712daf2a5607fab5536fdaa30ab3bb5
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2023-05-27 11:21:48 -0300

    atualizei projeto

--- style.js
+++ style.js
@@ -24,12 +24,29 @@ function ready() {
         var button = reomveCartButtons[i];
         button.addEventListener('click', removeCartItem);
     }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (var i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i];
+        input.addEventListener("change", quantityChanged);
+    }
 }
 
 function removeCartItem(event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
+    updatetotal();
 }
+
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updatetotal();
+
+}
+
 
 function updatetotal() {
     var cartContent = document.getElementsByClassName('cart-content')[0];
@@ -39,7 +56,10 @@ function updatetotal() {
         var cartBox = cartBoxes[i];
         var priceElement = cartBox.getElementsByClassName('cart-price')[0];
         var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        var price = parseFloat(priceElement.innerText.replace("$", ""));
         var quantity = quantityElement.value;
         total = total + price * quantity;
+
+        document.getElementsByClassName('total-price')[0].innerText = "$" + total;
     }
 }
